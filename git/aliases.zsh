0a7fbfc24f4ae76db5fe50d51f9f0a0242813f2a
Kyle Shipley <shipstar@gmail.com>
2015-01-11 12:48:26 -0500

    Update git config.

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -7,13 +7,13 @@ then
 fi
 
 # The rest of my fun git aliases
-alias gl='git pull --prune'
-alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
-alias gp='git push origin HEAD'
-alias gd='git diff'
+alias gs='git status'
+alias gl='git pull --rebase'
 alias gc='git commit'
-alias gca='git commit -a'
 alias gco='git checkout'
-alias gcb='git copy-branch-name'
 alias gb='git branch'
-alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias gd='git diff'
+alias gdc='git diff --cached'
+alias glgg='git log --color --graph --pretty=format:"%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr)%C(bold blue) <%an>%Creset"'
+alias gcb='git checkout -b'
+alias gpu='git push -u'
