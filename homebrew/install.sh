852fba1634b6ad51b841cb40ca1ea956e1e3c477
Kyle Shipley <shipstar@gmail.com>
2015-01-11 12:44:33 -0500

    Update homebrew installation with more packages + cask.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,18 +1,108 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
-# Check for Homebrew
-if test ! $(which brew)
-then
-  echo "  Installing Homebrew for you."
+# inspired by https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
+# and http://lapwinglabs.com/blog/hacker-guide-to-setting-up-your-mac
+
+# Install command-line tools using Homebrew.
+
+if test ! $(which brew); then
+  echo "Installing homebrew..."
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install homebrew packages
-brew install grc coreutils spark
+# Make sure we're using the latest Homebrew.
+brew update
 
-exit 0
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install GNU core utilities (those that come with OS X are outdated)
+# Donâ€™t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
+brew install findutils
+
+# Install zsh
+brew install zsh
+
+# Install more recent versions of some OSX tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+
+cli=(
+  curl
+  tree
+  watch
+)
+brew install ${cli[@]}
+
+# Install dev tools
+binaries=(
+  git
+  hub
+  leiningen
+  node
+  postgresql
+  rbenv
+  ruby-build
+)
+brew install ${binaries[@]}
+
+# Install brew services
+brew tap homebrew/boneyard
+
+# Install Mac apps
+brew install caskroom/cask/brew-cask
+
+# Access to betas for Chrome Canary, Sublime Text, etc
+brew tap caskroom/versions
+
+casks=(
+  alfred
+  # atom
+  caffeine
+  dropbox
+  firefox
+  flowdock
+  google-chrome
+  google-drive
+  iterm2
+  mailbox
+  mou
+  psequel
+  recordit
+  slack
+  sublime-text3
+  the-unarchiver
+  things
+  virtualbox
+  # xcode
+)
+
+brew cask install ${casks[@]}
+
+echo "Can't install:
+
+pixelmator
+reeder
+
+with casks. Use the Mac App Store."
+
+# Ensure that homebrew-cask apps are available to alfred
+brew cask alfred link
+
+# Install some fonts
+brew tap caskroom/fonts
+
+fonts=(
+  font-alfa-slab-one
+  font-podkova
+  font-poller-one
+  font-raleway
+  font-roboto
+  font-roboto-slab
+  font-yanone-kaffeesatz
+)
