429c37e54d74344b4aa0037c26bd93723364ca1b
Kyle Shipley <shipstar@gmail.com>
2015-04-06 19:51:28 -0400

    Export CHROME_BIN and FIREFOX_BIN for karma.

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,4 @@
 export EDITOR='atom'
+
+export CHROME_BIN='/opt/homebrew-cask/Caskroom/google-chrome/latest/Google Chrome.app/Contents/MacOS/Google Chrome'
+export FIREFOX_BIN='/opt/homebrew-cask/Caskroom/firefox/latest/Firefox.app/Contents/MacOS/firefox'
