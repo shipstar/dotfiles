61678d45f919d5cce0bba439703471d45e3b6472
shipstar <kyleashipley@gmail.com>
2017-10-10 11:55:51 -0400

    Fix bad quote.

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -17,7 +17,7 @@ brew update
 brew upgrade
 
 # Install GNU core utilities (those that come with OS X are outdated)
-# Donâ€™t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+# Don't forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
 brew install coreutils
 
 # Install GNU `find`, `locate`, `updatedb`, and `xargs`, g-prefixed
