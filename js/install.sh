7a4549b0080dcb13672be8a7f47d44cfc12f9865
shipstar <kyleashipley@gmail.com>
2017-10-23 19:30:36 -0400

    Install yarn as part of js installs.

--- js/install.sh
+++ js/install.sh
@@ -4,3 +4,8 @@ if test ! $(which n); then
   echo "Installing n (node version manager) for you"
   brew install n > /tmp/n-install.log
 fi
+
+if test ! $(which yarn); then
+  echo "Installing yarn"
+  brew install yarn > /tmp/yarn-install.log
+fi
