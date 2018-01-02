f41a83141ca69babe8d5ea3cc4e7ba2d65937916
shipstar <kyleashipley@gmail.com>
2018-01-02 16:42:42 -0500

    Remove OH aliases and add Woven one

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,3 @@
 alias reload!='. ~/.zshrc'
 
-alias gorms='cd ~/Programming/healthpro/rms'
-alias gop3a='cd ~/Programming/ourhealth/p3a'
-alias gop3p='cd ~/Programming/ourhealth/p3p'
+alias goapply='cd ~/Programming/woven/apply-yourself'
