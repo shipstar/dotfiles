7ba81f43f9aa014274782dc3cf5cb6bd2c95fa1c
Kyle Shipley <kyleashipley@gmail.com>
2018-07-22 11:47:50 -0400

    Use a menu when selecting autocompletion options

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# Use a menu if the number of items > 2
+zstyle ':completion:*' menu select=2
