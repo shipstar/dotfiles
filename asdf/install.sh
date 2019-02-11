5c07157ea9c3edf9af9299bc2993e1787cb8d8bf
Kyle Shipley <kyleashipley@gmail.com>
2019-02-11 17:55:41 -0500

    Install asdf from brew in the future instead

--- asdf/install.sh
+++ asdf/install.sh
@@ -2,13 +2,18 @@
 
 if test ! $(which asdf); then
   echo "Installing asdf (language version manager) for you"
-  git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.5.0 > /tmp/asdf-install.log
+  brew install asdf
 fi
 
 plugins=(
   nodejs
+  python
   ruby
 )
 for plugin in ${plugins[@]}; do
   asdf plugin-add $plugin
 done
+
+asdf install ruby 2.5.1
+asdf install python 3.7.2
+asdf install nodejs 11.9.0
