498e3ca85a425a8a6b59ff0fc8e5f2947399f6b9
Kyle Shipley <kyleashipley@gmail.com>
2019-05-29 18:08:08 -0400

    Fix path precedence issue with binstubs

--- system/_path.zsh
+++ system/_path.zsh
@@ -3,7 +3,6 @@
 # See http://superuser.com/questions/598810/zsh-config-to-export-or-not-to-export/598924#598924
 # for more info.
 path=(
-  ./bin
   /usr/local/bin
   /usr/local/sbin
   $ZSH/bin
@@ -15,5 +14,12 @@ path=(
 # for e.g. ruby, python, node
 # FIXME: There must be a better way to do this
 . $HOME/.asdf/asdf.sh
+
+# now make sure ./bin is at the front so things like binstubs
+# take precedence over system-wide libraries (e.g. bin/rake vs rake)
+path=(
+  ./bin
+  $path
+)
 
 export PATH
