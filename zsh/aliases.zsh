0600f20e3374cb909cb90ee7ec0ee3a7d781c9e1
Kyle Shipley <kyleashipley@gmail.com>
2020-04-14 14:32:57 -0400

    Remove unused aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,1 @@
 alias reload!='. ~/.zshrc'
-
-alias goapply='cd ~/Programming/woven/apply-yourself'
-alias gograde='cd ~/Programming/woven/graders'
-alias goexercises='cd ~/Programming/woven/exercises'
