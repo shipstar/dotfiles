4c9ac80a939d433a669be63704194e1df4c56140
Kyle Shipley <kyleashipley@gmail.com>
2020-05-13 10:17:18 -0400

    Remove obsolete env configuration

--- system/env.zsh
+++
@@ -1,4 +0,0 @@
-export EDITOR='atom'
-
-export CHROME_BIN='/usr/local/Caskroom/google-chrome/latest/Google Chrome.app/Contents/MacOS/Google Chrome'
-export FIREFOX_BIN='/usr/local/Caskroom/firefox/latest/Firefox.app/Contents/MacOS/firefox'
