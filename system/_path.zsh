757dcd2ca47589449bfc9382f4366238a8dba4f8
Kyle Shipley <kyleashipley@gmail.com>
2020-05-13 10:25:28 -0400

    Simplify path export

--- system/_path.zsh
+++ system/_path.zsh
@@ -2,19 +2,16 @@
 # Modifying one apparently modifies the other.
 # See http://superuser.com/questions/598810/zsh-config-to-export-or-not-to-export/598924#598924
 # for more info.
+
 path=(
   /usr/local/bin
   /usr/local/sbin
   $ZSH/bin
   ./node_modules/.bin
   $path
-)
-
-# now make sure ./bin is at the front so things like binstubs
-# take precedence over system-wide libraries (e.g. bin/rake vs rake)
-path=(
+  # make sure ./bin is at the front so things like binstubs
+  # take precedence over system-wide libraries (e.g. bin/rake vs rake)
   ./bin
-  $path
 )
 
 export PATH
