5770e0d5938ac3a79fc053e94a7fbfcec956d8e4
Kyle Shipley <kyleashipley@gmail.com>
2020-05-12 21:47:40 -0400

    Remove obsolete install pattern

--- heroku/install.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-brew tap heroku/brew && brew install heroku
-
-# Warning! May not work if `reload!` is not available at
-# this step of installation
-reload!
-# will prompt for Heroku login
-heroku autocomplete --refresh-cache
