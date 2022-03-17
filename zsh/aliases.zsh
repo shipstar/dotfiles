1b2dee01cfb9c18cb8e7d0ed70e4c7d9aa50e6d6
Kyle Shipley <kshipley@hellobrightline.com>
2022-03-17 09:52:33 -0400

    Add aliases

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,5 +1,9 @@
 alias reload!='. ~/.zshrc'
+# https://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
+alias sudo='sudo '
+alias vim="nvim"
+alias wds='webpack-dev-server'
 
+# directory aliases
 alias gobl="cd ~/Programming/brightline"
 alias goreef='cd ~/Programming/brightline/reef'
-alias wds='webpack-dev-server'
