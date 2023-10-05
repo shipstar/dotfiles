e6042d719d1b79ce01131364be223b34d5a108c2
Kyle Shipley <kshipley@hellobrightline.com>
2023-10-05 12:10:03 -0400

    Re-enable Homebrew auto-update

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,4 @@
 export EDITOR=$(which nvim)
-export HOMEBREW_NO_AUTO_UPDATE=1
 # temporary workaround for issue with krb5 + postgresql + pg gem
 # see https://github.com/ged/ruby-pg/issues/311
 export PGGSSENCMODE="disable"
