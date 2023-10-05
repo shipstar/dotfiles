80d4afc9c8adc4c9f2af3b5813984f9946ea078f
Kyle Shipley <kshipley@hellobrightline.com>
2023-10-05 12:09:31 -0400

    Fix pg issue

--- system/env.zsh
+++ system/env.zsh
@@ -1,2 +1,5 @@
 export EDITOR=$(which nvim)
 export HOMEBREW_NO_AUTO_UPDATE=1
+# temporary workaround for issue with krb5 + postgresql + pg gem
+# see https://github.com/ged/ruby-pg/issues/311
+export PGGSSENCMODE="disable"
