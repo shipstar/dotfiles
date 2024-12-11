a2380b4bdf90dc12ed5ca5c25b2f1cba3f5f2588
Kyle Shipley <kyleashipley@gmail.com>
2024-12-11 11:44:20 -0500

    Disable fork safety to fix errors on parallel tests

--- ruby/env.zsh
+++ ruby/env.zsh
@@ -1,1 +1,8 @@
 export RUBY_YJIT_ENABLE=1
+
+# Required to allow parallel testing in Rails 7
+# with whatever combination of gems I have installed at least.
+#
+# This is not a good long-term option, but there doesn't
+# seem to be a better workaround.
+export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
