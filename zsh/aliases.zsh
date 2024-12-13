51c3de5fe0958a00cba134a30f6458dedec94eb0
Kyle Shipley <kyleashipley@gmail.com>
2024-12-13 10:06:51 -0500

    Replace git aliases with 'jump' function j

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,8 +2,36 @@ alias reload!='. ~/.zshrc'
 # https://askubuntu.com/questions/22037/aliases-not-available-when-using-sudo
 alias sudo='sudo '
 alias vim="nvim"
-alias wds='webpack-dev-server'
 
-# directory aliases
-alias gobl="cd ~/Code/brightline"
-alias goreef='cd ~/Code/brightline/reef'
+# jump to common directories
+j() {
+  case "$1" in
+    "mb"|"mostlybits")
+      cd ~/Code/mostlybits
+      ;;
+    "hutch"|"hs"|"h")
+      cd ~/Code/mostlybits/hutch
+      ;;
+    "bl"|"brightline")
+      cd ~/Code/brightline
+      ;;
+    "reef")
+      cd ~/Code/brightline/reef
+      ;;
+    "ios"|"ica")
+      cd ~/Code/brightline/ios-caregiver-app
+      ;;
+    "android"|"aca")
+      cd ~/Code/brightline/android-caregiver-app
+      ;;
+    *)
+      echo "Unknown location. Available shortcuts:"
+      echo "  mb, mostlybits -> ~/Code/mostlybits"
+      echo "  hutch, hs, h   -> ~/Code/mostlybits/hutch"
+      echo "  bl, brightline -> ~/Code/brightline"
+      echo "  reef           -> ~/Code/brightline/reef"
+      echo "  ios, ica       -> ~/Code/brightline/ios-caregiver-app"
+      echo "  android, aca   -> ~/Code/brightline/android-caregiver-app"
+      ;;
+  esac
+}
