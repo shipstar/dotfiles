82a39a267d100675585b8b380f561b0846077fe8
Kyle Shipley <kshipley@hellobrightline.com>
2025-03-04 15:42:10 -0500

    Commit postgres config that fixes segfault

--- system/env.zsh
+++ system/env.zsh
@@ -1,1 +1,4 @@
 export EDITOR="$(which code) --wait"
+# temporary workaround for issue with krb5 + postgresql + pg gem
+# see https://github.com/ged/ruby-pg/issues/311
+export PGGSSENCMODE="disable"
